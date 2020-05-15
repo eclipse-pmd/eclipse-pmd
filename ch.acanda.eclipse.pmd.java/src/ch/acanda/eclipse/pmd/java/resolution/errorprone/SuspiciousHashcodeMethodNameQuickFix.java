d13f46c1a31b8a70e638be1bf9c01c490ce312b4
Philip Graf <git@acanda.ch>
2020-05-15 23:56:11 +0200

    Remove copyright statement from file headers

--- ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/resolution/errorprone/SuspiciousHashcodeMethodNameQuickFix.java
+++ ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/resolution/errorprone/SuspiciousHashcodeMethodNameQuickFix.java
@@ -1,14 +1,3 @@
-// =====================================================================
-//
-// Copyright (C) 2012 - 2020, Philip Graf
-//
-// All rights reserved. This program and the accompanying materials
-// are made available under the terms of the Eclipse Public License v1.0
-// which accompanies this distribution, and is available at
-// http://www.eclipse.org/legal/epl-v10.html
-//
-// =====================================================================
-
 package ch.acanda.eclipse.pmd.java.resolution.errorprone;
 
 import org.eclipse.jdt.core.dom.CompilationUnit;
