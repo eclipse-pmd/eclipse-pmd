c1f44866fcbd23a17acb269b137276e3a51069c9
Philip Graf <git@acanda.ch>
2023-02-03 16:45:13 +0100

    Add a project's project dependencies to type resolution

--- ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/classloader/JavaClassLoaderProvider.java
+++ ch.acanda.eclipse.pmd.java/src/ch/acanda/eclipse/pmd/java/classloader/JavaClassLoaderProvider.java
@@ -4,14 +4,14 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
@@ -33,7 +33,6 @@ public class JavaClassLoaderProvider implements PMDClassLoaderProvider {
         return Optional.empty();
     }
 
-
     private static class ClassLoaderCache {
 
         private final Map<String, CachedClassLoader> cache = new HashMap<>();
@@ -51,53 +50,65 @@ public class JavaClassLoaderProvider implements PMDClassLoaderProvider {
         }
 
         private ClassLoader getClassLoader(final IFile file) {
-            final IJavaProject project = JavaCore.create(file.getProject());
-            if (project != null) {
+            final IProject project = file.getProject();
+            final Collection<IPath> projects = new ArrayList<>();
+            projects.add(project.getFullPath());
+            final URL[] classpath = getClasspath(project, projects).toArray(URL[]::new);
+            return classpath.length == 0 ? null : new URLClassLoader(classpath);
+        }
+
+        private Stream<URL> getClasspath(final IProject project, final Collection<IPath> projects) {
+            final IJavaProject javaProject = JavaCore.create(project);
+            if (javaProject != null) {
                 try {
-                    final List<URL> urls = new ArrayList<>();
-                    urls.add(getDefaultOutputLocation(project, file));
-                    urls.addAll(getClasspathEntryOutputLocations(project));
-                    return new URLClassLoader(urls.toArray(URL[]::new));
-                } catch (final MalformedURLException | JavaModelException e) {
+                    final Stream<URL> defaultOutput = Stream.of(getDefaultOutputLocation(javaProject));
+                    final Stream<URL> entries = getClasspathEntries(javaProject, projects);
+                    return Stream.concat(defaultOutput, entries);
+                } catch (final JavaModelException | MalformedURLException e) {
                     // TODO: log exception
+                }
+            }
+            return Stream.empty();
+        }
+
+        private Stream<URL> getClasspathEntries(final IJavaProject project, final Collection<IPath> projects)
+                throws JavaModelException, MalformedURLException {
+            return Stream.of(project.getResolvedClasspath(true)).flatMap(entry -> getClassesLocation(entry, project, projects));
+        }
+
+        private Stream<URL> getClassesLocation(final IClasspathEntry entry, final IJavaProject project, final Collection<IPath> projects) {
+            final int kind = entry.getEntryKind();
+            if (kind == IClasspathEntry.CPE_SOURCE) {
+                return toURL(entry.getOutputLocation());
+            }
+            if (kind == IClasspathEntry.CPE_LIBRARY) {
+                return toURL(entry.getPath());
+            }
+            if (kind == IClasspathEntry.CPE_PROJECT) {
+                final IProject cpProject =
+                        project.getProject().getWorkspace().getRoot().getProject(entry.getPath().toString());
+                if (!projects.contains(cpProject.getFullPath())) {
+                    projects.add(cpProject.getFullPath());
+                    return getClasspath(cpProject, projects);
                 }
             }
             return null;
         }
 
-        private List<URL> getClasspathEntryOutputLocations(final IJavaProject project)
-                throws JavaModelException, MalformedURLException {
-            return Stream.of(project.getResolvedClasspath(true))
-                    .map(entry -> getClassesLocation(entry))
-                    .filter(Objects::nonNull)
-                    .map(loc -> toURL(loc))
-                    .filter(Objects::nonNull)
-                    .toList();
+        private Stream<URL> toURL(final IPath path) {
+            if (path != null) {
+                try {
+                    return Stream.of(path.toFile().getAbsoluteFile().toURI().toURL());
+                } catch (final MalformedURLException e) {
+                    // TODO: log exception
+                }
+            }
+            return Stream.empty();
         }
 
-        private IPath getClassesLocation(final IClasspathEntry entry) {
-            final int kind = entry.getEntryKind();
-            if (kind == IClasspathEntry.CPE_SOURCE) {
-                return entry.getOutputLocation();
-            }
-            if (kind == IClasspathEntry.CPE_LIBRARY) {
-                return entry.getPath();
-            }
-            return null;
-        }
-
-        private URL toURL(final IPath path) {
-            try {
-                return path.toFile().getAbsoluteFile().toURI().toURL();
-            } catch (final MalformedURLException e) {
-                return null;
-            }
-        }
-
-        private URL getDefaultOutputLocation(final IJavaProject project, final IFile file)
-                throws MalformedURLException, JavaModelException {
-            return file.getWorkspace()
-                    .getRoot()
+        private URL getDefaultOutputLocation(final IJavaProject project) throws MalformedURLException, JavaModelException {
+            return project.getProject()
+                    .getWorkspace().getRoot()
                     .getFolder(project.getOutputLocation())
                     .getRawLocation().toFile().toURI().toURL();
         }
