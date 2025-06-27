1c03f2ad30cf291e6f1f116259ad7fdd951d83d4
Philip Graf <git@acanda.ch>
2025-06-27 20:33:11 +0200

    Add new target 2025-06 and remove the unsupported target 2024-06

--- ch.acanda.eclipse.pmd.core.swtbot/src/ch/acanda/eclipse/pmd/swtbot/bot/PMDPreferenceDialogBot.java
+++ ch.acanda.eclipse.pmd.core.swtbot/src/ch/acanda/eclipse/pmd/swtbot/bot/PMDPreferenceDialogBot.java
@@ -1,9 +1,20 @@
 package ch.acanda.eclipse.pmd.swtbot.bot;
 
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.anyOf;
+import static org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory.withMnemonic;
+
+import java.util.List;
+
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.hamcrest.Matcher;
 
 public final class PMDPreferenceDialogBot extends DialogBot {
+
+    // Eclipse 2025-03 and earlier use "Preferences", 2025-06 uses "Preferences..."
+    private static final Matcher<MenuItem> PREFERENCES =
+            anyOf(List.of(withMnemonic("Preferences"), withMnemonic("Preferences...")));
 
     private PMDPreferenceDialogBot(final Shell shell) {
         super(shell);
@@ -11,7 +22,7 @@ public final class PMDPreferenceDialogBot extends DialogBot {
 
     public static PMDPreferenceDialogBot open() {
         final SWTWorkbenchBot bot = new SWTWorkbenchBot();
-        bot.menu("Window").menu("Preferences").click();
+        bot.menu("Window").menu(PREFERENCES, false, 0).click();
         final PMDPreferenceDialogBot preferenceBot = new PMDPreferenceDialogBot(bot.shell("Preferences").widget);
         preferenceBot.bot().tree().getTreeItem("PMD").select();
         return preferenceBot;
